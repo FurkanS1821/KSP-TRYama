e6ec3171148ff8a50e178bca3af7943168501d03
FurkanS1821 <furkan.sam.1821@gmail.com>
2022-04-10 17:20:22 +0300

    %2.74

---
+++ convertToJson.bat
@@ -0,0 +1,4 @@
+KSPCFG2JSON "dictionary - Orijinal.cfg"
+KSPCFG2JSON "dictionary - Serenity.cfg"
+KSPCFG2JSON "Kerbal Space Program\GameData\Squad\Localization\dictionary.cfg"
+KSPCFG2JSON "Kerbal Space Program\GameData\SquadExpansion\Serenity\Localization\dictionary.cfg"
