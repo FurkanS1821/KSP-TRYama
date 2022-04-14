4556ec782528388df7769929ec4836e5ea6591a1
FurkanS1821 <furkan.sam.1821@gmail.com>
2022-04-14 04:53:15 +0300

    %7.4 - Parça özelliklerinin yarısı

--- commit.bat
+++ commit.bat
@@ -1,8 +1,9 @@
 @echo off
+call convertToCfg.bat
+echo.
 git status
 git add *
 git status
-echo.
 Diff "Base Game & MH" "Kerbal Space Program\GameData\Squad\Localization" "{0}\dictionary.cfg" "{0}\..\..\..\..\dictionary - Orijinal.cfg" "BG" "Kerbal Space Program\GameData\SquadExpansion\Serenity\Localization" "{0}\dictionary.cfg" "{0}\..\..\..\..\..\dictionary - Serenity.cfg"
 set /p msg="Enter commit message: "
 git commit -m "%msg%"
